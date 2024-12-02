ec337f2d49e79b58e54809df3cbaa1382fa7bba9
moldenburger <maritoldenburger@hotmail.com>
2024-12-02 17:13:22 +0100

    Huiswerk geüpload.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
+        Integer[] numeric = {1,2,3,4,5,6,7,8,9,0};
+        String[] alphabetic = {"Eén", "Twee", "Drie", "Vier", "vijf", "Zes", "Zeven", "Acht", "Negen", "Nul"};
 
+        Translator translator = new Translator(alphabetic,numeric);
 
+        System.out.println(translator.translate(6));
+        Scanner scanner = new Scanner(System.in);
+
+        boolean play = true;
+
+        while (play) {
+            System.out.println("Type 'x' om te stoppen \nType 'v' om te vertalen");
+            String input = scanner.nextLine();
+            if (input.equals("x")) {
+                play = false;
+            } else if (input.equals("v")) {
+                System.out.println("Type een cijfer in van 0 t/m 9");
+                Integer number = scanner.nextInt();
+                System.out.println("De vertaling van " + number + " is " + translator.translate(number));
+                scanner.nextLine();
+            } else {
+                System.out.println("Ongeldig");
+            }
+        }
     }
 }
